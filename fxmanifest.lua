ec6632f1d1a026db65c0c5f33c29ebc6fa502e59
Alvin Veroy <me@alvin.tech>
2020-06-24 21:28:50 +0800

    Fixed for esx 1.2 modified for lockdown studio server (WIP)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,10 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+
+game 'gta5'
 
 description 'ESX Unicorn Job'
 
-version '1.1.0'
+version '1.2.0'
 
 client_scripts {
   '@es_extended/locale.lua',
